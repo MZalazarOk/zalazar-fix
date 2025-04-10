074a6289ead455667bead13a1e7e060570b9aa2f
Mariano Zalazar <82074313+MZalazarOk@users.noreply.github.com>
2025-04-10 16:13:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ document.querySelector('#app').innerHTML = `
   <h1>Zalazar Fix</h1>
   <p>Aplicación para quitar filtros de fotos.</p>
 `;
+// Registro del Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
