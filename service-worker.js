da4dce646b31750e27386480b37f91e6a3b40579
Mariano Zalazar <82074313+MZalazarOk@users.noreply.github.com>
2025-04-10 16:23:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
 self.addEventListener('install', event => {
+  console.log('Zalazar Fix instalado');
   self.skipWaiting();
 });
 
+self.addEventListener('activate', event => {
+  console.log('Zalazar Fix activo');
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  // Estrategia simple para cache si querés después
 });
